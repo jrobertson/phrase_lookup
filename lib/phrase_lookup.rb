55428db1f056c7f150bfa5889b8d57506f2111a8
James Robertson <digital.robertson@gmail.com>
2022-02-23 19:04:18 +0000

    improvement: Using the new RXFreadWrite module which replaces rxfhelper.

--- lib/phrase_lookup.rb
+++ lib/phrase_lookup.rb
@@ -6,127 +6,128 @@
 
 
 require 'yaml'
-require 'rxfhelper'
+require 'rxfreadwrite'
 
 
 class PhraseLookup
 
-  def initialize(obj=nil, debug: false)  
-    
+  def initialize(obj=nil, debug: false)
+
     @debug = debug
 
     @master = if obj then
-      
+
       if obj.is_a? String then
-        
-        s = RXFHelper.read(obj).first
-        
+
+        s = RXFReader.read(obj).first
+
         if s.lstrip =~ /^---/ then # it's YAML
-          
+
           Master.new( YAML.load(s))
-          
+
         elsif s =~ /: /   # it's the contents of a log file
-          
+
           m = Master.new
           m.create s
           m
-          
+
         else   # it's plain text
-          
+
           m = Master.new
           m.create_from_txt s
           m
-          
+
         end
-        
+
       elsif obj.is_a? Array
-        
+
         puts 'array detected' if @debug
-        
+
         m = Master.new
         m.parse_array obj
         m
-        
+
       else
         Master.new obj
       end
-      
+
     end
-    
+
 
   end
-  
-  class Master    
-    
+
+  class Master
+    include RXFReadWriteModule
+
     def initialize(h={})
 
       @h = h
-      
+
     end
-    
+
     def create(s)
-      
+
       @h = parse_log(s)
-      
+
     end
-    
+
     def create_from_txt(s)
-      
+
       @h = parse(s)
-      
-    end    
-    
+
+    end
+
     def update(s)
 
       parse_log(s).each {|k,v| @h.has_key?(k) ? @h[k] += v : @h[k] = v }
 
     end
-    
+
     def save(filename='master.yaml')
-      File.write filename, @h.sort.to_h.to_yaml
+      FileX.write filename, @h.sort.to_h.to_yaml
     end
-    
+
     def to_h()
       @h.clone
-    end    
-    
+    end
+
     def parse(raw_s)
-      
-      s, _ = RXFHelper.read(raw_s)       
-      
+
+      s, _ = RXFReader.read(raw_s)
+
       a = s.downcase.gsub(/\?/,'').lines.map(&:strip).compact
       a.uniq.inject({}) {|r,x| r.merge x => a.count(x)}
-      
-    end    
-    
+
+    end
+
     def parse_array(a)
-           
+
       @h = a.map {|x| x.downcase.gsub(/\?/,'')}\
           .inject({}) {|r,x| r.merge x => a.count(x)}
-      
-    end 
-    
+
+    end
+
     def parse_log(raw_s)
-      
-      s, _ = RXFHelper.read(raw_s)       
-      
-      a = s.downcase.gsub(/\?/,'').lines.map do |x| 
+
+      s, _ = RXFReader.read(raw_s)
+
+      a = s.downcase.gsub(/\?/,'').lines.map do |x|
         phrase = x.strip[/(?<=: )[^|]+(?= )/i]
         phrase.length < 50 ? phrase : nil if phrase
       end.compact
-      
+
       a.uniq.inject({}) {|r,x| r.merge x => a.count(x)}
-      
+
     end
   end
 
   def lookup(s, limit: 10, search_tags: false)
-    
+
     return [] if s.empty?
-    
+
     h = @master.to_h
     a = h.keys
-    
+
     a3 = if search_tags then
       a.grep /\].*(?:\b#{s}\b|\b#{s}).*\|.*(?:\b#{s}\b|\b#{s})/i
     else
@@ -137,12 +138,12 @@ class PhraseLookup
     end
 
     return a3.sort_by {|word| -h[word]}.map {|x| x.sub(/ +\|.*$/,'')}\
-        .uniq.take(limit)        
-    
+        .uniq.take(limit)
+
   end
 
   alias q lookup
-  
+
   def master()
     @master
   end
--- phrase_lookup.gemspec
+++ phrase_lookup.gemspec
@@ -1,13 +1,13 @@
 Gem::Specification.new do |s|
   s.name = 'phrase_lookup'
-  s.version = '0.1.9'
+  s.version = '0.2.0'
   s.summary = 'Returns a list of matching phrases from plain text'
   s.authors = ['James Robertson']
   s.files = Dir['lib/phrase_lookup.rb']
-  s.add_runtime_dependency('rxfhelper', '~> 1.0', '>=1.0.0')
+  s.add_runtime_dependency('rxfreadwrite', '~> 0.2', '>=0.2.4')
   s.signing_key = '../privatekeys/phrase_lookup.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/phrase_lookup'
 end
